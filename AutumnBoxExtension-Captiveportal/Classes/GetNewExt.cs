480c4656d5b0c673045e71ccaecc349c5cefae39
MonoLogueChi <xxwhite.com@gmail.com>
2018-08-10 22:35:11 +0800

    确定更新方式

--- AutumnBoxExtension-Captiveportal/Classes/GetNewExt.cs
+++ AutumnBoxExtension-Captiveportal/Classes/GetNewExt.cs
@@ -1,35 +1,27 @@
 ﻿using System.IO;
 using System.Net;
 using System.Diagnostics;
+using AutumnBox.Basic.Executer;
 
 namespace AutumnBoxExtension_Captiveportal.Classes
 {
+    //临时更新方案
     public class GetNewExt
     {
         public void DownLoadFiles(string url, string path)
         {
-            var NewCaptiveportal = "https://dl.sm9.top/AutumuBox/Extension/Captiveportal/bat/NewCaptiveportal.bat";
+            var newCaptiveportalBat = "https://dl.sm9.top/AutumuBox/Extension/Captiveportal/bat/NewCaptiveportal.bat";
 
             WebClient client = new WebClient();
-            client.DownloadFile(NewCaptiveportal, Path.Combine(path, "NewCaptiveportal.bat"));
+            client.DownloadFile(newCaptiveportalBat, Path.Combine(path, "NewCaptiveportal.bat"));
             client.DownloadFile(url, Path.Combine(path, "AutumnBoxExtension-Captiveportal.dll"));
         }
 
+
         public void StartCmd(string path)
         {
-            Process process = new Process();
-
-            // 初始化可执行文件的一些基础信息
-            process.StartInfo.WorkingDirectory = Path.Combine(path); // 初始化可执行文件的文件夹信息
-            process.StartInfo.FileName = "NewCaptiveportal.bat"; // 初始化可执行文件名
-
-            process.StartInfo.UseShellExecute = true;        // 使用操作系统shell启动进程
-
-            // 启动可执行文件
-            process.Start();
+            new CommandExecuter().Execute(Command.MakeForCmd("Start "+ Path.Combine(path, "NewCaptiveportal.bat")));
         }
-
-
 
     }
 }
